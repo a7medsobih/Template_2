d1bceca0267b22926d6356b51213f3804b2d032e
a7medsobih <ahmedsobih7@gamil.com>
2024-04-24 19:48:33 +0200

    Created Slider

--- assets/JS/jQuery_main.js
+++ assets/JS/jQuery_main.js
@@ -1,5 +1,47 @@
-$(document).ready(function(){
-    $(".protfolio li").click(function(){
+$(document).ready(function () {
+
+    $(".header ul li a").hover(function () {
+        $(".header ul li a").removeClass("navActive");
+        $(this).addClass("navActive");
+    });
+
+    $(".protfolio li").click(function () {
         $(this).addClass("active").siblings().removeClass("active");
     });
-})
+
+    var list =$(".header nav ul");
+
+    $(".toggle-menu").click(function(e){
+        list.toggleClass("hidden");
+        e.stopPropagation();    // تمنع انتشار الحدث إلى العناصر الأخرى
+    });
+    
+    $("body").click(function(){
+        list.removeClass("hidden");
+    });
+    
+    list.on({
+        click: function (e) {
+            e.stopPropagation();   // تمنع انتشار الحدث إلى العناصر الأخرى
+        },
+    });
+
+
+
+
+
+
+
+
+    $(window).scroll(function(){
+        var sc =$(window).scrollTop();
+        // console.log(sc);
+
+       
+    });
+
+
+
+
+
+});
