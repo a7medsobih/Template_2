69dc59ee91747e2cee7adaa525c3858e258c966c
a7medsobih <ahmedsobih7@gamil.com>
2024-04-28 23:07:15 +0300

    to updade

--- assets/JS/jQuery_main.js
+++ assets/JS/jQuery_main.js
@@ -5,43 +5,98 @@ $(document).ready(function () {
         $(this).addClass("navActive");
     });
 
-    $(".protfolio li").click(function () {
-        $(this).addClass("active").siblings().removeClass("active");
+    $("nav li a").click(function (e) {
+        e.preventDefault();     // -> بتمنع التصرف الافتراضى
+        // console.log('.' + $(this).data('scroll'));
+
+        // scrolling navbar
+        $('html ,body').animate({
+            scrollTop: $("." + $(this).data('scroll')).offset().top + 1
+        }, 1000);
     });
 
-    var list =$(".header nav ul");
+    $(window).on("scroll", function () {
 
-    $(".toggle-menu").click(function(e){
+        var sc = $(window).scrollTop();
+        // console.log(sc);
+        var buttonUp = $(".buttonUp");
+        if (sc >= 634) {
+
+            buttonUp.fadeIn();
+        } else {
+            buttonUp.fadeOut();
+
+        }
+
+    });
+
+    // scroll to top 
+    $(".buttonUp").click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+    var list = $(".header nav ul");
+
+    $(".toggle-menu").click(function (e) {
         list.toggleClass("hidden");
         e.stopPropagation();    // تمنع انتشار الحدث إلى العناصر الأخرى
     });
-    
-    $("body").click(function(){
+
+    $("body").click(function () {
         list.removeClass("hidden");
     });
-    
+
     list.on({
         click: function (e) {
             e.stopPropagation();   // تمنع انتشار الحدث إلى العناصر الأخرى
         },
     });
 
+    //slider
+    var images = ['04.jpg', '00.jpg','01.jpg','03.jpg','05.jpg','06.jpg']; // اسماء الصور
+    var bullets = ['circle1', 'circle2', 'circle3','circle4','circle5','circle6'];  // موضع الصوره
 
+    var currentIndex = bulletsIndex = 1; // الصورة الحالية
 
+    function changeBackground() {
+        $('.landing').css('background-image', 'url(assets/images/' + images[currentIndex] + ')');
+        $(".bullets i").eq(bulletsIndex).addClass("active").siblings().removeClass("active");
+    }
 
+    // تغيير الخلفية إلى الصورة الأولى عند تحميل الصفحة
+    changeBackground();
 
+    $('.bullets i').click(function () {
+        var clickedIndex = $(this).index();
+        currentIndex = clickedIndex;
+        bulletsIndex = clickedIndex;
+        $('.landing').fadeOut('fast', function () {
+            changeBackground();
+            $('.landing').fadeIn('fast');
+        });
 
-
-
-    $(window).scroll(function(){
-        var sc =$(window).scrollTop();
-        // console.log(sc);
-
-       
     });
 
+    $('.left').click(function () {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        bulletsIndex = (bulletsIndex - 1 + bullets.length) % bullets.length;    //تغيير موضع الدائره الزرقاء
+        $('.landing').fadeOut('fast', function () {
+            changeBackground();
+            $('.landing').fadeIn('fast');
+        });
+    });
 
+    $('.right').click(function () {
+        currentIndex = (currentIndex + 1) % images.length;
+        bulletsIndex = (bulletsIndex + 1 + bullets.length) % bullets.length;  //تغيير موضع الدائره الزرقاء
+        $('.landing').fadeOut('fast', function () {
+            changeBackground();
+            $('.landing').fadeIn('fast');
+        });
+    });
 
-
+    
 
 });
